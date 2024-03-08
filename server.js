b1ea2019e10023421c759c30c2199e8515df3e5f
Joe Lyles <lylesjoe@protonmail.com>
2024-03-08 04:24:18 -0500

    imported errorHandler and completed middleware section in course

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -74,14 +75,19 @@ app.get('/hello(.html)?', (req, res, next) => {
     res.send('hello world!');
 });
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// changed from app.use('/*',.....) to app.all('*',.....) ~2h55m in course
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+    res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
 
 // error function added with cors
-app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send(err.message);
-})
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
