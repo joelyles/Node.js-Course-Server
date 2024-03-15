faab2fdb5ab311bbccbe3c7dafb0540dbb542a03
Joe Lyles <lylesjoe@protonmail.com>
2024-03-15 05:20:35 -0400

    followed course to update controllers for CRUD operations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,12 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) {this.users = data }
-}
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
-
 const jwt = require('jsonwebtoken');
-const fsPromises = require('fs');
-const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.sendStatus(400).json({ 'message': 'username and password are required.' });
-    const foundUser = userDB.users.fund(person => person.username === user);
+    
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401);
     // eval pwd
     const match = await bcrypt.compare(pwd, foundUser.password);
@@ -34,15 +29,12 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' }
         );
         // the following saves refresh token with current user
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+
         // refresh token as http only is more secure
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure: true,
         res.json({ accessToken });
     } else {
         res.sendStatus(401);
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,22 +1,18 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) {this.users = data }
-}
-
+const User = require('../model/User');
 const jwt = require('jsonwebtoken');
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
-    const foundUser = userDB.users.fund(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) return res.sendStatus(403);
     // eval jwt
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
-        (err, decode) => {
+        (err, decoded) => {
             if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
             const roles = Object.values(foundUser.roles);
             const accessToken = jwt.sign(
